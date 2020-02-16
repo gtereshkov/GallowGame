947ab8cfb46314ea0ad25ca23b6f4f3295f147ff
kempelBLR <glebtereshkov@gmail.com>
2020-02-16 23:06:49 +0300

    Realized 'Restart' functionality + prettified letters containers

--- js/index.js
+++ js/index.js
@@ -5,14 +5,14 @@ const restartGameButton = document.getElementById('restart-game');
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-var initialHiddenWords = ['KAMASUTRA', 'ATMOSPHERE', 'BASKETBALL', 'COMPLETION', 'COMPLIANCE', 'FOUNDATION', 'INSTRUMENT', 'TECHNIQUES'];
+let initialHiddenWords = ['KAMASUTRA', 'ATMOSPHERE', 'BASKETBALL', 'COMPLETION', 'COMPLIANCE', 'FOUNDATION', 'INSTRUMENT', 'TECHNIQUES'];
 
 function pickRandomWord(wordsArray) {
     return wordsArray[Math.floor(Math.random() * wordsArray.length)]
 };
 
 function createLetterContainer(className, whereToCreate, whatToInsert) {
-    var div = document.createElement('div');
+    let div = document.createElement('div');
     div.setAttribute('class', className);
     if (whatToInsert) {
         div.innerHTML = whatToInsert;
@@ -21,58 +21,86 @@ function createLetterContainer(className, whereToCreate, whatToInsert) {
 }
 
 function renderHiddenWordContainer(hiddenWord) {
-    for (var i = 0; i<=hiddenWord.length - 1; i++) {
+    for (let i = 0; i<=hiddenWord.length - 1; i++) {
         createLetterContainer('hidden-letter-container', hiddenWordContainer);
     }
 };
 
 function renderKeyboard (lettersArray) {
-    for (var i = 0; i<=lettersArray.length - 1; i++) {
+    for (let i = 0; i<=lettersArray.length - 1; i++) {
         createLetterContainer('keyboard-letter-container', keyboardContainer, lettersArray[i])
     }
 };
 
-startGameButton.addEventListener('click', function() {
-        var wordToGuess = pickRandomWord(initialHiddenWords);
-        console.log(wordToGuess);   
-        renderHiddenWordContainer(wordToGuess);
-        renderKeyboard(alphabet);
+function generalGameLogic() {
+    startGameButton.disabled = true;
+    let wordToGuess = pickRandomWord(initialHiddenWords);
+    console.log(wordToGuess);   
+    renderHiddenWordContainer(wordToGuess);
+    renderKeyboard(alphabet);
 
-        guessedWordLettersContainers = document.querySelectorAll('.hidden-letter-container');
-        keyboardLetters = document.querySelectorAll('.keyboard-letter-container');
-        let guessedLettersCount = 0;
-        keyboardLetters.forEach(function(elem) {
-            elem.addEventListener('click', function(e) {                
-                let pressedLetter = e.target.innerText;
-                console.log(e.target.innerText);
-                
-                let matchingLettersPositions = [];
-                for (var i = 0; i<=wordToGuess.length - 1; i++) {
-                    if (pressedLetter == wordToGuess[i]){
-                        matchingLettersPositions.push(i + 1);
-                    };
+    let guessedWordLettersContainers = document.querySelectorAll('.hidden-letter-container');
+    let keyboardLetters = document.querySelectorAll('.keyboard-letter-container');
+    let guessedLettersCount = 0;
+    keyboardLetters.forEach(function(elem) {
+        elem.addEventListener('click', function(e) {                
+            let pressedLetter = e.target.innerText;
+            console.log(e.target.innerText);
+            
+            let matchingLettersPositions = [];
+            for (let i = 0; i<=wordToGuess.length - 1; i++) {
+                if (pressedLetter == wordToGuess[i]){
+                    matchingLettersPositions.push(i + 1);
                 };
+            };
 
-                guessedLettersCount += matchingLettersPositions.length;
-                console.log("ugadano: " + guessedLettersCount, "vsegobukv: " + wordToGuess.length);
+            guessedLettersCount += matchingLettersPositions.length;
+            console.log("ugadano: " + guessedLettersCount, "vsegobukv: " + wordToGuess.length);
 
-                if (matchingLettersPositions.length > 0) {
-                    for (var i = 0; i<=matchingLettersPositions.length-1;i++){
-                        console.log(matchingLettersPositions);
-                        document.querySelector('.hidden-letter-container:nth-child(' + matchingLettersPositions[i] +')').innerHTML = pressedLetter;
-                    };
-                }
+            if (matchingLettersPositions.length > 0) {
+                for (let i = 0; i<=matchingLettersPositions.length-1;i++){
+                    console.log(matchingLettersPositions);
+                    document.querySelector('.hidden-letter-container:nth-child(' + matchingLettersPositions[i] +')').innerHTML = pressedLetter;
+                };
+            }
 
-                if (guessedLettersCount == wordToGuess.length) (
-                    guessedWordLettersContainers.forEach(function(elem){
-                            console.log(elem);
-                            elem.setAttribute("style", "background-color:#2cbd4e; color: red; font-size: 24px; padding-top: 5px");
-                        }
-                    )
+            if (guessedLettersCount == wordToGuess.length) (
+                guessedWordLettersContainers.forEach(function(elem){
+                        console.log(elem);
+                        elem.setAttribute("style", "background-color:#2cbd4e; color: red; font-size: 24px; padding-top: 5px");
+                    }
                 )
-            })
+            )
         })
-        
-    }
-);
+    })    
+};
+
+startGameButton.addEventListener('click', generalGameLogic);
+
+restartGameButton.addEventListener("click", function() {
+    swal({
+        title: "Are you sure want to restart the game?",
+        text: "Once restarted, you will loose your current progress!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+    .then((willDelete) => {
+        if (willDelete) {
+            swal("Poof! Try with another word", {
+                icon: "success",
+            });
+            hiddenWordContainer.innerHTML = "";
+            keyboardContainer.innerHTML = "";
+            generalGameLogic();
+        } else {
+            swal("Keep guessing current word!");
+        }
+    })
+}
+);  
+
+
+
+
 
