ac2cf0101b44009ddf89001e22adda6f60185920
kempelBLR <glebtereshkov@gmail.com>
2020-02-16 03:02:18 +0300

    realized words guessing functional

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const hiddenWordContainer = document.getElementById('hidden-word-container');
 const keyboardContainer = document.getElementById('keyboard-container');
 const startGameButton = document.getElementById('start-game');
+const restartGameButton = document.getElementById('restart-game');
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
@@ -37,15 +38,38 @@ startGameButton.addEventListener('click', function() {
         renderHiddenWordContainer(wordToGuess);
         renderKeyboard(alphabet);
 
+        guessedWordLettersContainers = document.querySelectorAll('.hidden-letter-container');
         keyboardLetters = document.querySelectorAll('.keyboard-letter-container');
+        let guessedLettersCount = 0;
         keyboardLetters.forEach(function(elem) {
-            elem.addEventListener('click', function(e) {
+            elem.addEventListener('click', function(e) {                
+                let pressedLetter = e.target.innerText;
                 console.log(e.target.innerText);
-                for (var i = 0; i<=wordToGuess.length - 1; i++){
-                    if (wordToGuess.indexOf(e.target.innerText) > 0) {
-                        document.querySelector('#hidden-word-container:nth-child(' + wordToGuess.indexOf(e.target.innerText) +')').innerHTML = e.target.innerText;
-                    }
+                
+                let matchingLettersPositions = [];
+                for (var i = 0; i<=wordToGuess.length - 1; i++) {
+                    if (pressedLetter == wordToGuess[i]){
+                        matchingLettersPositions.push(i + 1);
+                    };
+                };
+
+                guessedLettersCount += matchingLettersPositions.length;
+                console.log("ugadano: " + guessedLettersCount, "vsegobukv: " + wordToGuess.length);
+
+                if (matchingLettersPositions.length > 0) {
+                    for (var i = 0; i<=matchingLettersPositions.length-1;i++){
+                        console.log(matchingLettersPositions);
+                        document.querySelector('.hidden-letter-container:nth-child(' + matchingLettersPositions[i] +')').innerHTML = pressedLetter;
+                    };
                 }
+
+                if (guessedLettersCount == wordToGuess.length) (
+                    guessedWordLettersContainers.forEach(function(elem){
+                            console.log(elem);
+                            elem.setAttribute("style", "background-color:#2cbd4e; color: red; font-size: 24px; padding-top: 5px");
+                        }
+                    )
+                )
             })
         })
         
